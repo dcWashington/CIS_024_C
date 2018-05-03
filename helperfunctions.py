b96b3bcbfd7c75a9672c551d1812f086298f4710
dcWashington <35947001+dcWashington@users.noreply.github.com>
2018-05-03 09:43:48 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,15 @@
+
+def add(x,y):
+    return x+y
+
+def diff(x,y):
+    return x-y
+
+def product(x,y):
+    return x*y
+
+def greatest(x,y):
+    if x>y:
+        return x;
+    else:
+        return y;
